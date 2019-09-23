5c14a3dbad03312da0ac9ec77b524749ff174408
Bronze Law <bronzelaw3@gmail.com>
2019-09-23 20:07:03 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,7 +8,7 @@ def introduction(name)
 end
 language = "Ruby"
 def intorduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in {Ruby}"
+  puts "Hi, my name is #{name} and I am learning to program in {language}"
 
 
 
