7485b4f4e408719c7de9f9c11a482d4e8d879ec8
Bronze Law <bronzelaw3@gmail.com>
2019-09-23 20:12:24 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,7 +4,7 @@
 # in order to complete the rest of this course and be ready for the next course!
 
 def introduction(name)
-  puts "Hi, My name is #{name}."
+ puts "Hi, my name is #{name}."
 end
 
 def intorduction_with_language(name, language)
